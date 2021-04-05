300b41aba813f20e335b13de34673f795ab10da2
red1_amd <61431379+RedwanAhmedd@users.noreply.github.com>
2021-04-05 18:25:00 +0600

    #19 Completed inflation of first layout

--- Theory/Assignment/Assignment02/app/src/main/java/com/example/assignment/MainActivity.java
+++ Theory/Assignment/Assignment02/app/src/main/java/com/example/assignment/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.assignment;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+     EditText name;
+     EditText id;
+     EditText phone;
+     EditText nid;
+     Button next;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        name = findViewById(R.id.editText_name);
+        id = findViewById(R.id.editText_id);
+        phone = findViewById(R.id.editText_phone);
+        nid = findViewById(R.id.editText_nid);
+        next = findViewById(R.id.button_next);
+
+
     }
 }
