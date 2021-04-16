d09797762a9eb9bfba361834536a0368a0a93e1a
red1_amd <61431379+RedwanAhmedd@users.noreply.github.com>
2021-04-16 21:14:18 +0600

    #16 created entity class

---
+++ Theory/Assignment/Assignment02/app/src/main/java/com/example/assignment/entity/StudentInfo.java
@@ -0,0 +1,106 @@
+package com.example.assignment.entity;
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "student_info")
+public class StudentInfo {
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "id")
+    private final Integer studentID;
+
+    @ColumnInfo(name = "name")
+    private String studentName;
+
+    @ColumnInfo(name = "school")
+    private String schoolName;
+
+    @ColumnInfo(name = "department")
+    private String departmentName;
+
+    @ColumnInfo(name = "date of birth")
+    private Integer dateOfBirth;
+
+    @ColumnInfo(name = "phone")
+    private Integer phoneNumber;
+
+    @ColumnInfo(name = "NID")
+    private Integer nID;
+
+    public StudentInfo(@NonNull Integer studentID
+            , String studentName
+            , String  schoolName
+            , String  departmentName
+            , @NonNull Integer dateOfBirth
+            , @NonNull Integer phoneNumber
+            , @NonNull Integer nID){
+        this.studentID = studentID;
+        this.studentName = studentName;
+        this.schoolName = schoolName;
+        this.departmentName = departmentName;
+        this.dateOfBirth = dateOfBirth;
+        this.phoneNumber = phoneNumber;
+        this.nID = nID;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public void setSchoolName(String schoolName) {
+        this.schoolName = schoolName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    public void setDateOfBirth(Integer dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public void setPhoneNumber(Integer phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setNID(Integer nID) {
+        this.nID = nID;
+    }
+
+
+
+
+
+    @NonNull
+    public Integer getStudentID() {
+        return studentID;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public String getSchoolName() {
+        return schoolName;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public Integer getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public Integer getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public Integer getNID() {
+        return nID;
+    }
+
+}
