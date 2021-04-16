e71d2ddd0c21bd5bcdcceb1e14b7799fc1675b75
red1_amd <61431379+RedwanAhmedd@users.noreply.github.com>
2021-04-16 21:37:01 +0600

    #17 created dao interface

---
+++ Theory/Assignment/Assignment02/app/src/main/java/com/example/assignment/dao/StudentInfoDao.java
@@ -0,0 +1,43 @@
+package com.example.assignment.dao;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.example.assignment.entity.StudentInfo;
+
+import java.util.List;
+
+@Dao
+public interface StudentInfoDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void insert(StudentInfo studentInfo);
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void insertAll(StudentInfo... studentInfos);
+
+    @Query("SELECT id FROM student_info ORDER BY id ASC")
+    LiveData<List<Integer>> getAllIds();
+
+    @Query("SELECT * FROM student_info ORDER BY id ASC")
+    LiveData<List<StudentInfo>> getAllStudents();
+
+    @Query("SELECT * FROM student_info WHERE id LIKE :studentID LIMIT 1")
+    LiveData<StudentInfo> getStudent(Integer studentID);
+
+    @Update
+    void update(StudentInfo studentInfo);
+
+    @Update
+    void updateAll(StudentInfo... studentInfo);
+
+    @Delete
+    void delete(StudentInfo studentInfo);
+
+    @Delete
+    void deleteAll(StudentInfo... studentInfos);
+}
