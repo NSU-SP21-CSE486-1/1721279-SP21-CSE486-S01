5ac7e21ab5215a9427799159fff06bf241aa54c0
red1_amd <61431379+RedwanAhmedd@users.noreply.github.com>
2021-04-26 02:26:54 +0600

    #41 fired the intent

--- Theory/Assignment/Assignment03/FirebaseDBV01/app/src/main/java/com/example/application/MainActivity.java
+++ Theory/Assignment/Assignment03/FirebaseDBV01/app/src/main/java/com/example/application/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.application;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+     Button studentAdd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        studentAdd = findViewById(R.id.button_main_add);
+        studentAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                nextActivity();
+            }
+        });
+
+    }
+
+    private void nextActivity() {
+        Intent intent = new Intent(this,AddStudent.class);
+        startActivity(intent);
     }
 }
