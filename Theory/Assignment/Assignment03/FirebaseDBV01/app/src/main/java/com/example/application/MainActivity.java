52bbfc49902c9f946f8714ec6239cca023b0bf14
red1_amd <61431379+RedwanAhmedd@users.noreply.github.com>
2021-04-30 23:01:50 +0600

    #36 view student button now takes users to the recyclerview activity

--- Theory/Assignment/Assignment03/FirebaseDBV01/app/src/main/java/com/example/application/MainActivity.java
+++ Theory/Assignment/Assignment03/FirebaseDBV01/app/src/main/java/com/example/application/MainActivity.java
@@ -8,18 +8,34 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-     Button studentAdd;
+    Button studentAdd;
+    Button studentView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         studentAdd = findViewById(R.id.button_main_add);
+        studentView = findViewById(R.id.button_main_view);
+
         studentAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 nextActivity();
             }
         });
+        studentView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewActivity();
+            }
+        });
+
+
+    }
+
+    private void viewActivity() {
+        Intent intent = new Intent(this,ViewStudent.class);
+        startActivity(intent);
 
     }
 
