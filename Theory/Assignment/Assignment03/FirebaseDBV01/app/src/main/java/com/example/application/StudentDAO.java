c406464b1550cbff9b5575f8776ad1d9fa1ee8d8
red1_amd <61431379+RedwanAhmedd@users.noreply.github.com>
2021-05-04 20:10:41 +0600

    #42 edited studentDao class

--- Theory/Assignment/Assignment03/FirebaseDBV01/app/src/main/java/com/example/application/StudentDAO.java
+++ Theory/Assignment/Assignment03/FirebaseDBV01/app/src/main/java/com/example/application/StudentDAO.java
@@ -3,6 +3,7 @@ package com.example.application;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 
 public class StudentDAO {
     private final DatabaseReference databaseReference;
@@ -16,5 +17,9 @@ public class StudentDAO {
     public Task<Void> add (Students student){
 
        return databaseReference.push().setValue(student);
+    }
+    public Query get(){
+
+        return databaseReference.orderByKey();
     }
 }
