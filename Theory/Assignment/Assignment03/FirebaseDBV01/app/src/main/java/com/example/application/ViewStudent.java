eab83e27295c8fff12885e56f97c39693cd0d196
red1_amd <61431379+RedwanAhmedd@users.noreply.github.com>
2021-05-04 19:58:13 +0600

    #48 created adapter class

--- Theory/Assignment/Assignment03/FirebaseDBV01/app/src/main/java/com/example/application/ViewStudent.java
+++ Theory/Assignment/Assignment03/FirebaseDBV01/app/src/main/java/com/example/application/ViewStudent.java
@@ -2,13 +2,57 @@ package com.example.application;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class ViewStudent extends AppCompatActivity {
-
+    SwipeRefreshLayout swipeRefreshLayout;
+    RecyclerView recyclerView;
+    Adapter adapter;
+    StudentDAO dao;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_student);
+        recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        LinearLayoutManager manager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(manager);
+        adapter = new Adapter(this);
+        recyclerView.setAdapter(adapter);
+
+         dao = new StudentDAO();
+         loadData();
+    }
+
+    private void loadData() {
+
+        dao.get().addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                ArrayList<Students> students = new ArrayList<>();
+                for(DataSnapshot data : snapshot.getChildren()){
+                   Students student = data.getValue(Students.class);
+                   students.add(student);
+                }
+                adapter.setItems(students);
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
