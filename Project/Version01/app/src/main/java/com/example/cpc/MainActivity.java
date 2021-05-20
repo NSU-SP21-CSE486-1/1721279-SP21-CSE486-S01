f2254ef973cc369f5f22293fbea155b67fee374d
red1_amd <61431379+RedwanAhmedd@users.noreply.github.com>
2021-05-20 23:52:14 +0600

    #52 completed the layout

--- Project/Version01/app/src/main/java/com/example/cpc/MainActivity.java
+++ Project/Version01/app/src/main/java/com/example/cpc/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.cpc;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
-
+     Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.button_start);
+    }
+
+
+    public void nextActivity(View view) {
     }
 }
