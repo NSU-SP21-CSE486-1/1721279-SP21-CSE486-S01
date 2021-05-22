794c93a6ae9f31eeee8b75e3c0a997582cce0a43
red1_amd <61431379+RedwanAhmedd@users.noreply.github.com>
2021-05-22 12:17:35 +0600

    #59 implemented check field

--- Project/Version01/app/src/main/java/com/example/cpc/LoginActivity.java
+++ Project/Version01/app/src/main/java/com/example/cpc/LoginActivity.java
@@ -1,14 +1,53 @@
 package com.example.cpc;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
-
+    EditText email,password;
+    Button loginBtn,gotoRegister;
+    boolean valid = true;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        email = findViewById(R.id.loginEmail);
+        password = findViewById(R.id.loginPassword);
+        loginBtn = findViewById(R.id.loginBtn);
+        gotoRegister = findViewById(R.id.gotoRegister);
+
+
+
+       loginBtn.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               checkField(email);
+               checkField(password);
+
+           }
+       });
+
+
+
+    }
+
+
+
+
+    public boolean checkField(EditText textField){
+        if(textField.getText().toString().isEmpty()){
+            textField.setError("Error");
+            valid = false;
+        }else {
+            valid = true;
+        }
+
+        return valid;
     }
 }
