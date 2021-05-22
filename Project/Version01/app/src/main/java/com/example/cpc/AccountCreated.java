218f586093df81d5577b5c8ab99726697b4a7ecd
red1_amd <61431379+RedwanAhmedd@users.noreply.github.com>
2021-05-22 23:10:28 +0600

    #62 AccountCreated activity takes users to login page

--- Project/Version01/app/src/main/java/com/example/cpc/AccountCreated.java
+++ Project/Version01/app/src/main/java/com/example/cpc/AccountCreated.java
@@ -1,14 +1,25 @@
 package com.example.cpc;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AccountCreated extends AppCompatActivity {
-
+    Button gotoLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_created);
+        gotoLogin = findViewById(R.id.gotoLoginBtn);
+
+        gotoLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+            }
+        });
     }
 }
