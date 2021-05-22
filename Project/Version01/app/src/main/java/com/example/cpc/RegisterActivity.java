97827655b3d66556f141d1a3218bf88690a4f3ab
red1_amd <61431379+RedwanAhmedd@users.noreply.github.com>
2021-05-22 12:39:36 +0600

    #59 implemented checkfield
    register activity

--- Project/Version01/app/src/main/java/com/example/cpc/RegisterActivity.java
+++ Project/Version01/app/src/main/java/com/example/cpc/RegisterActivity.java
@@ -1,14 +1,50 @@
 package com.example.cpc;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RegisterActivity extends AppCompatActivity {
+    EditText fullName,email,password,phone;
+    Button registerBtn,goToLogin;
+    boolean valid = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        fullName = findViewById(R.id.registerName);
+        email = findViewById(R.id.registerEmail);
+        password = findViewById(R.id.registerPassword);
+        phone = findViewById(R.id.registerPhone);
+        registerBtn = findViewById(R.id.registerBtn);
+        goToLogin = findViewById(R.id.gotoLogin);
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkField(fullName);
+                checkField(email);
+                checkField(password);
+                checkField(phone);
+            }
+        });
+    }
+
+
+
+    public boolean checkField(EditText textField){
+        if(textField.getText().toString().isEmpty()){
+            textField.setError("Error");
+            valid = false;
+        }else {
+            valid = true;
+        }
+
+        return valid;
     }
 }
