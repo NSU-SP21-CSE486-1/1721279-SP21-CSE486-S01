99ceb07848d903317cef2689268f547b6febb220
red1_amd <61431379+RedwanAhmedd@users.noreply.github.com>
2021-05-22 12:29:43 +0600

    #58 explicit intent
    also some modifications done on register activity layout.

--- Project/Version01/app/src/main/java/com/example/cpc/LoginActivity.java
+++ Project/Version01/app/src/main/java/com/example/cpc/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.cpc;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -30,6 +31,13 @@ public class LoginActivity extends AppCompatActivity {
                checkField(email);
                checkField(password);
 
+           }
+       });
+
+       gotoRegister.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+             startActivity(new Intent(getApplicationContext(),RegisterActivity.class));
            }
        });
 
