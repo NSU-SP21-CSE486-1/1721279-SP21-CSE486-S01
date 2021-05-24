3f76ee5f4fab2970d42a620438a46e59b4be4bf8
red1_amd <61431379+RedwanAhmedd@users.noreply.github.com>
2021-05-24 23:52:38 +0600

    #66 users are granted different access during login

--- Project/Version01/app/src/main/java/com/example/cpc/LoginActivity.java
+++ Project/Version01/app/src/main/java/com/example/cpc/LoginActivity.java
@@ -2,21 +2,37 @@ package com.example.cpc;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class LoginActivity extends AppCompatActivity {
-    EditText email,password;
-    Button loginBtn,gotoRegister;
+    EditText email, password;
+    Button loginBtn, gotoRegister;
     boolean valid = true;
-    
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
 
         email = findViewById(R.id.loginEmail);
         password = findViewById(R.id.loginPassword);
@@ -24,38 +40,78 @@ public class LoginActivity extends AppCompatActivity {
         gotoRegister = findViewById(R.id.gotoRegister);
 
 
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-       loginBtn.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View v) {
-               checkField(email);
-               checkField(password);
+                checkField(email);
+                checkField(password);
 
-           }
-       });
+                if (valid) {
 
-       gotoRegister.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View v) {
-             startActivity(new Intent(getApplicationContext(),RegisterActivity.class));
-           }
-       });
+                    fAuth.signInWithEmailAndPassword(email.getText().toString(), password.getText().toString()).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                        @Override
+                        public void onSuccess(AuthResult authResult) {
+                            checkUserAccessLevel(authResult.getUser().getUid());
 
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(LoginActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+                }
+            }
+        });
+
+        gotoRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), RegisterActivity.class));
+            }
+        });
 
 
     }
 
+    private void checkUserAccessLevel(String uid) {
+
+        DocumentReference df = fStore.collection("Users").document(uid);
+        df.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                Log.d("TAG","OnSuccess: + " + documentSnapshot.getData());
+
+                // identify the user access level
+                if (documentSnapshot.getString("isAdmin") != null) {
+                    // Admin Access Granted
+                    startActivity(new Intent(getApplicationContext(), AdminDashboard.class));
+                    finish();
+
+                }
+                if (documentSnapshot.getString("isUser") != null) {
+                    // User Access Granted
+                    startActivity(new Intent(getApplicationContext(), Dashboard.class));
+                    finish();
+
+                }
+            }
+        });
+
+    }
 
 
-
-    public boolean checkField(EditText textField){
-        if(textField.getText().toString().isEmpty()){
+    public boolean checkField(EditText textField) {
+        if (textField.getText().toString().isEmpty()) {
             textField.setError("Error");
             valid = false;
-        }else {
+        } else {
             valid = true;
         }
 
         return valid;
     }
+
 }
