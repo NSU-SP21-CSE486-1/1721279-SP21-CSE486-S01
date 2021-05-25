439a249ef7c1e742cbebf1426181f6ef31fb50d0
red1_amd <61431379+RedwanAhmedd@users.noreply.github.com>
2021-05-26 02:23:39 +0600

    #71 created fragment for home

--- Project/Version01/app/src/main/java/com/example/cpc/Dashboard.java
+++ Project/Version01/app/src/main/java/com/example/cpc/Dashboard.java
@@ -1,19 +1,28 @@
 package com.example.cpc;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 
-public class Dashboard extends AppCompatActivity {
+public class Dashboard extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     DrawerLayout drawerLayout;
     NavigationView navigationView;
     ActionBarDrawerToggle toggle;
     Toolbar toolbar;
+    FragmentManager fragmentManager;
+    FragmentTransaction fragmentTransaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +33,7 @@ public class Dashboard extends AppCompatActivity {
         getSupportActionBar().setTitle("Dashboard");
         navigationView = findViewById(R.id.navigationView);
         drawerLayout = findViewById(R.id.drawer);
+        navigationView.setNavigationItemSelectedListener(this);
 
 
         toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close);
@@ -32,5 +42,41 @@ public class Dashboard extends AppCompatActivity {
         toggle.syncState();
 
 
+        //Load default fragment
+        fragmentManager = getSupportFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.container_fragment, new Home());
+        fragmentTransaction.commit();// add the fragment
+
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        drawerLayout.closeDrawer(GravityCompat.START);
+        if (item.getItemId() == R.id.profile) {
+            fragmentManager = getSupportFragmentManager();
+            fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.container_fragment, new Profile());
+            fragmentTransaction.commit();// add the fragment
+        }
+        if (item.getItemId() == R.id.saved_jobs) {
+            fragmentManager = getSupportFragmentManager();
+            fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.container_fragment, new SavedJobs());
+            fragmentTransaction.commit();// add the fragment
+        }
+        if (item.getItemId() == R.id.home) {
+            fragmentManager = getSupportFragmentManager();
+            fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.container_fragment, new Home());
+            fragmentTransaction.commit();// add the fragment
+        }
+        if (item.getItemId() == R.id.logout) {
+            FirebaseAuth.getInstance().signOut();
+            startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+            finish();
+        }
+        return true;
     }
 }
