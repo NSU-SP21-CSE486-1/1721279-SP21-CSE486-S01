0ccd56848c2e651ced63b8e6fd6a5ff921a5602f
red1_amd <61431379+RedwanAhmedd@users.noreply.github.com>
2021-05-25 23:40:25 +0600

    #65 minor modification

--- Project/Version01/app/src/main/java/com/example/cpc/AdminDashboard.java
+++ Project/Version01/app/src/main/java/com/example/cpc/AdminDashboard.java
@@ -1,14 +1,30 @@
 package com.example.cpc;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class AdminDashboard extends AppCompatActivity {
+    Button logoutBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_dashboard);
+        logoutBtn = findViewById(R.id.logoutBtn1);
+
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                finish();
+            }
+        });
     }
 }
