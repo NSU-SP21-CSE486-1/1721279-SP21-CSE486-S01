1abd4077c04a1d5cef2ef2bb590e9cfe7078b53a
red1_amd <61431379+RedwanAhmedd@users.noreply.github.com>
2021-05-25 12:45:38 +0600

    #69 navigation drawer for users implemented

--- Project/Version01/app/src/main/java/com/example/cpc/Dashboard.java
+++ Project/Version01/app/src/main/java/com/example/cpc/Dashboard.java
@@ -2,13 +2,35 @@ package com.example.cpc;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class Dashboard extends AppCompatActivity {
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    ActionBarDrawerToggle toggle;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Dashboard");
+        navigationView = findViewById(R.id.navigationView);
+        drawerLayout = findViewById(R.id.drawer);
+
+
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.setDrawerIndicatorEnabled(true);
+        toggle.syncState();
+
+
     }
 }
